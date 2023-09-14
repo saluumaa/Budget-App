68fd3daa7b80abeb6153b2142619a5bfaa822179
saluumaa <salumahassan02@gmail.com>
2023-09-14 21:38:08 +0300

    Change uploaded files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,10 +4,7 @@
 Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
-Rails.application.config.assets.paths << Rails.root.join('node_modules')
-
-# Precompile additional assets.
-Rails.application.config.assets.precompile += %w( additional_asset.js additional_asset.css )
+# Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
